1fc7a5233e81b899b818bbf89623e1bce5d43923
vishwajitvm <vishwajitmall50@gmail.com>
2024-05-19 03:37:49 +0530

    learn - to create, update , delete file and also learn how to get values from paremter passedd from console

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Learning node js basic!")
+const fs = require('fs') ;
+
+/* 
+arg0 - node path in system
+arg1 - project path
+arg2 - paramter from console
+arg3 - parameter from console
+*/
+const input = process.argv ;
+if(input[2] == "ADD" ) {
+    fs.writeFileSync(input[3] , input[4])
+}
+else if(input[2] == "UPDATE" ) {
+    fs.writeFileSync(input[3] , input[4])
+}
+else if(input[2] == "REMOVE") {
+    fs.unlinkSync(input[3])
+}
+else {
+    console.log("Invalid paramter") ;
+}
