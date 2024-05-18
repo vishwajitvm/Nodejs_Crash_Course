d57588d0505950872ecf38850e0fb0317cc6ee47
vishwajitvm <vishwajitmall50@gmail.com>
2024-05-19 03:00:59 +0530

    start: learn to create node server and listedn on 4500 port and , learn to create basic static api

---
+++ data.js
@@ -0,0 +1,17 @@
+const data = [{
+    name: "vishwajit vm" ,
+    email: "vishwajitvm@gmail.com"
+},
+{
+    name: "Ramesh" ,
+    email: "Ramesh@gmail.com"
+},
+{
+    name: "Rajiv" ,
+    email: "Rajiv@gmail.com"
+},
+{
+    name: "Rajpal" ,
+    email: "Rajpal@gmail.com"
+}]
+module.exports = data ;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Learning node js basic!")
+const http = require("http") ;
+const data = require("./data");
+
+http.createServer((req , res) => {
+    res.writeHead(200 , { 'content-Type': 'application/json' })
+    res.write(JSON.stringify(data)) ;
+    res.end() ;
+}).listen(5000) ;
