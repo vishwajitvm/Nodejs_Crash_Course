8ec0baf9ac4dbe1fcae2a525b7d9c0d10c6e3aab
vishwajitvm <vishwajitmall50@gmail.com>
2024-05-21 12:14:49 +0530

    learn basic routing in intro

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require("express") ;
 const app = express() ;
+
+app.get("" , (req , res) => {
+    res.send(`Hello , this is home page ${req.query.name}`) ;
+})
+
+app.get("/about" , (req , res) => {
+    res.send("Hello , this is about page") ;
+})
+
+app.listen(4200)
