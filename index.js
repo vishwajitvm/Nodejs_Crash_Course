0fb589a593477a897a00380e99cf8149449208db
vishwajitvm <vishwajitmall50@gmail.com>
2024-05-23 04:32:40 +0530

    upload file using multer

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const express = require("express") ;
+const multer = require("multer") ;
 const app = express() ;
+
+const upload = multer({
+    storage:multer.diskStorage({
+        destination: function(request , file , callback) {
+            callback(null , "upload_file_here")
+        },
+
+        filename:function(req , file , callback){
+            callback(null , file.fieldname+"_"+Date.now() + ".jpg")
+        }
+    })
+}).single("upload_file_input_name") ;
+
+//ROUTES
+app.post("/upload" , upload , (req, res) => {
+    res.send("File Upload") ;
+})
 
 app.listen(4500) ;
