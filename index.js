9c73165459bc94b8a8ac501af068b5c565100eab
vishwajitvm <vishwajitmall50@gmail.com>
2024-05-23 13:57:42 +0530

    learn event emit and  event lister - learn event emitter

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const express = require("express") ;
+const EventEmitter = require("events") ;
 const app = express() ;
 
+//EVENT EMIITER instance
+const event = new EventEmitter() ;
+let count = 0 ;
 
+//EVENT LISTER
+event.on("COUNT" , () => {
+    count++ ;
+    console.log("EVENT CALLED ", count) ;
+})
+
+
+app.get("/" , (req, res) => {
+    res.send("This is home page");
+    event.emit("COUNT") ; //EMIT A COUNT EVENT
+})
+
+app.get("/about" , (req, res) => {
+    res.send("This is about page")
+    event.emit("COUNT") ; //EMIT A COUNT EVENT
+})
+
+app.get("/contact" , (req, res) => {
+    res.send("This is contact page")
+    event.emit("COUNT") ; //EMIT A COUNT EVENT
+})
+
+app.get("/history" , (req, res) => {
+    res.send("This is history page")
+})
 
 app.listen(4500) ;
