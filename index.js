7350cd08fe655801c968a8db71ed96e0d0310aab
vishwajitvm <vishwajitmall50@gmail.com>
2024-05-25 02:00:28 +0530

    mysql connections

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const express = require("express") ;
-const app = express() ;
+const mysql = require("mysql") ;
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+}) ;
+
+//DATABASE CONNECTION
+con.connect((err) => {
+    if(err) {
+        console.log("502:DATABASE CONNECTION ERROR")
+    }
+    else{
+        console.log("DATABASE CONNECTION SUCCESSFULL")
+    }
+}) ;
 
 
 
-app.listen(4500) ;
+
+
