15e120dfd985b8cfedfc2de3a3b84104cceae1ad
vishwajitvm <vishwajitmall50@gmail.com>
2024-05-26 06:22:19 +0530

    created common config for datbase connection

---
+++ config.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql") ;
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+}) ;
+
+//DATABASE CONNECTION
+con.connect((err) => {
+    if(err) {
+        console.log("502:DATABASE CONNECTION ERROR")
+    }
+    else{
+        console.log("DATABASE CONNECTION SUCCESSFULL")
+    }
+}) ;
+
+module.exports = con ;
